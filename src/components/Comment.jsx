6765c088a3f0ac7f05216696f08ee642e957a769
Paulo Ricardo <notadead5@gmail.com>
2023-07-02 15:32:04 -0300

    Avatar, comment and post

---
+++ src/components/Comment.jsx
@@ -0,0 +1,34 @@
+import { ThumbsUp, Trash } from 'phosphor-react';
+import {Avatar} from './Avatar'
+import styles from './Comment.module.css';
+
+export function Comment () {
+    return (
+        <div className={styles.comment}>
+            <Avatar hasBorder={false} src="https://github.com/pauloRRRRR.png" alt="" />
+            <div className={styles.commentBox}>
+                <div className={styles.commentContent}>
+                    <header>
+                        <div className={styles.authorAndTime}>
+                            <strong>Paulo Ricardo</strong>
+                            <time title='02 de Julho ás 13:06' dateTime='2023-02-07 13:06:30' >Cerca de 1h atrás</time>
+    
+                        </div>
+                        <button title='Deletar comentário'> 
+                            <Trash size={24} />
+                        </button>
+                    </header>
+                    <p>
+                        Muito bom Laura, parabens!
+                    </p>    
+                </div>
+                <footer>
+                    <button>
+                        <ThumbsUp/>
+                        Aplaudiar <span>20</span>
+                    </button>
+                </footer>
+            </div>
+        </div>
+    )
+}
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,6 +1,7 @@
 import {PencilLine} from 'phosphor-react'
 
 import styles from './Sidebar.module.css'
+import { Avatar } from './Avatar'
 
 export function Sidebar() {
     return (
@@ -10,7 +11,7 @@ export function Sidebar() {
                 src="https://images.unsplash.com/photo-1510784722466-f2aa9c52fff6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=300&q=70" alt="" />
 
             <div className={styles.profile}>
-                <img className={styles.avatar} src="https://github.com/PauloRRRRR.png"/>
+                <Avatar src="https://github.com/PauloRRRRR.png" />
                 <strong>Paulo Ricardo</strong>
                 <span>Web Developer</span>
             </div>
