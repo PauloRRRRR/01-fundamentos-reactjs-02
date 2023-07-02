aa4d7eb91ce35f6ba3326095dbbcc6b4adbc639d
Paulo Ricardo <notadead5@gmail.com>
2023-07-02 20:14:05 -0300

    Post component

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,51 @@ import { Sidebar } from './components/Sidebar';
 import styles from './App.module.css'; 
 import './global.css'
 
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/PauloRRRRR.png',
+      name: 'Paulo Ricardo',
+      role: 'QA Analyst'
+    },
+    content: [
+      {type: 'paragraph', content: 'Fala galera'},
+      {type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifólio'},
+      {type: 'link', content: 'jane.desing/doctorcare'},
+    ],
+    publishedAt: new Date('2023-05-03 20:00:00'),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://github.com/lauraandrade00.png',
+      name: 'Laura Andrade',
+      role: 'Frontend Dev'
+    },
+    content: [
+      {type: 'paragraph', content: 'Hihi, eu sou Laura'},
+      {type: 'paragraph', content: 'Dá uma sacada no meu projeto'},
+      {type: 'link', content: 'jane.desing/doctorcare'},
+    ],
+    publishedAt: new Date('2023-06-03 20:00:00'),
+  },
+  {
+    id: 3,
+    author: {
+      avatarUrl: 'https://github.com/PauloRRRRR.png',
+      name: 'Paulo Ricardo',
+      role: 'QA Analyst'
+    },
+    content: [
+      {type: 'paragraph', content: 'Fala galera'},
+      {type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifólio'},
+      {type: 'link', content: 'jane.desing/doctorcare'},
+    ],
+    publishedAt: new Date('2023-05-10 20:00:00'),
+  },
+];
+
 export function App(){
   return(
     <div>
@@ -12,7 +57,15 @@ export function App(){
       <div className={styles.wrapper}>
         <Sidebar/> 
         <main>
-          <Post/>
+          {posts.map(post => {
+            return (
+              <Post 
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            )
+          })}
         </main>
       </div>
     </div>
